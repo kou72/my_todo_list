979038e57eb911ef35f16256ff12018371c36f71
kou72 <genpokopokopoko@gmail.com>
2020-08-27 00:43:44 +0900

    comment out

--- public/main.js
+++ public/main.js
@@ -7,61 +7,60 @@ new Vue({
     isLoggedIn: false,
   },
 
-  // TODO
-  // created: function () {
-  //   firebase.auth().onAuthStateChanged((user) => {
-  //     this.user = user ? user : null;
-  //     this.isLoggedIn = user ? true : false;
-  //     if (!user) return;
-
-  //     const documentRef = firebase.firestore().collection("users").doc(this.user.uid).collection("todos");
-
-  //     documentRef
-  //       .where("done", "==", false)
-  //       .get()
-  //       .then((snapshot) => {
-  //         snapshot.forEach((doc) => {
-  //           this.todos.push(doc.data());
-  //         });
-  //       });
-  //   });
-  // },
+  created: function () {
+    // TODO.3
+    // firebase.auth().onAuthStateChanged((user) => {
+    //   this.user = user ? user : null;
+    //   this.isLoggedIn = user ? true : false;
+      // TODO.4
+      // if (!user) return;
+      // const documentRef = firebase.firestore().collection("users").doc(this.user.uid).collection("todos");
+      // documentRef
+      //   .where("done", "==", false)
+      //   .get()
+      //   .then((snapshot) => {
+      //     snapshot.forEach((doc) => {
+      //       this.todos.push(doc.data());
+      //     });
+      //   });
+    // });
+  },
 
   methods: {
     addItem: function () {
       // TODO.2
-      if (!this.text) return;
-      const item = {
-        id: Math.random().toString(32).substring(2), // ランダム文字列をIDに設定
-        todo: this.text,
-        done: false,
-      };
-      this.todos.push(item);
-      this.text = "";
-      // TODO
+      // if (!this.text) return;
+      // const item = {
+      //   id: Math.random().toString(32).substring(2), // ランダム文字列をIDに設定
+      //   todo: this.text,
+      //   done: false,
+      // };
+      // this.todos.push(item);
+      // this.text = "";
+      // TODO.4
       // if (this.isLoggedIn) this.insertDb(item);
     },
 
     doneItem: function (item) {
       // TODO.2
-      item.done = true;
-      // TODO
+      // item.done = true;
+      // TODO.4
       // if (this.isLoggedIn) this.updateDb(item);
     },
 
     login: function () {
-      // // TODO
+      // TODO.3
       // const provider = new firebase.auth.GoogleAuthProvider();
       // firebase.auth().signInWithRedirect(provider);
     },
 
     logout: function () {
-      // // TODO
+      // TODO.3
       // firebase.auth().signOut();
     },
 
     insertDb: function (item) {
-      // // TODO
+      // TODO.4
       // firebase.firestore().collection("users").doc(this.user.uid).collection("todos").doc(item.id).set({
       //   id: item.id,
       //   todo: item.todo,
@@ -71,7 +70,7 @@ new Vue({
     },
 
     updateDb: function (item) {
-      // // TODO
+      // TODO.4
       // firebase.firestore().collection("users").doc(this.user.uid).collection("todos").doc(item.id).update({
       //   id: item.id,
       //   todo: item.todo,
